d1819668ebe2ab6bb7dad50c35485ad22e783bbd
David Young-chan Kay <davidykay@gmail.com>
2015-03-25 19:14:18 -0400

    added datepicker but getting a crash.

--- src/clojure/com/davidykay/helloclojure/main.clj
+++ src/clojure/com/davidykay/helloclojure/main.clj
@@ -8,6 +8,39 @@
            (android.app DatePickerDialog DatePickerDialog$OnDateSetListener)
            (android.app DialogFragment)))
 
+(defn date-picker [activity]
+  (proxy [DialogFragment DatePickerDialog$OnDateSetListener] []
+    (onCreateDialog [savedInstanceState]
+      (let [c (Calendar/getInstance)
+            year (.get c Calendar/YEAR)
+            month (.get c Calendar/MONTH)
+            day (.get c Calendar/DAY_OF_MONTH)]
+        (DatePickerDialog. activity this year month day)))
+     (onDateSet [view year month day]
+       (set-elmt ::date
+         (format "%d%02d%02d" year (inc month) day)))))
+
+(defn show-picker [activity dp]
+  (. dp show (. activity getFragmentManager) "datePicker"))
+
+(defn main-layout [activity]
+  [:linear-layout {:orientation :vertical,
+                   :id-holder :true,
+                   :def `mylayout}
+   [:edit-text {:hint "Event name",
+                :id ::name}]
+   [:edit-text {:hint "Event location",
+                :id ::location}]
+   [:linear-layout {:orientation :horizontal}
+    [:text-view {:hint "Event date",
+                 :id ::date}]
+    [:button {:text "...",
+              :on-click (fn [_] (show-picker activity
+                                             (date-picker activity)))}]]
+   [:button {:text "+ Event",
+             :on-click (fn [_](add-event))}]
+   [:text-view {:text @listing,
+                :id ::listing}]])
 
 (declare ^android.widget.LinearLayout mylayout)
 
@@ -26,22 +59,9 @@
   (set-elmt ::name ""))
 
 (defn add-event []
-  (swap! listing str (get-elmt ::location) " - " 
-           (get-elmt ::name) "\n")
+  (swap! listing str 
+     (apply format "%d - %s - %s\n" (map get-elmt [::date ::location ::name])))
   (update-ui))
-
-
-(def main-layout [:linear-layout {:orientation :vertical,
-                                  :id-holder true,
-                                  :def `mylayout}
-                  [:edit-text {:hint "Event name",
-                               :id ::name}]
-                  [:edit-text {:hint "Event location",
-                               :id ::location}]
-                  [:button {:text "+ Event",
-                            :on-click (fn [_] (add-event))}]
-                  [:text-view {:text @listing,
-                              :id ::listing}]])
 
 
 (defactivity com.davidykay.helloclojure.MainActivity
@@ -50,6 +70,6 @@
   (fn [this bundle]
     (on-ui
      (set-content-view! (*a)
-                        main-layout))
+                        (main-layout this)))
     (on-ui
      (set-elmt ::listing @listing))))
